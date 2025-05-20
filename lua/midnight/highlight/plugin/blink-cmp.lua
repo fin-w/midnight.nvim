6121fbe755b59aa421666c5fdb94577fe79050c9
fin-w <fin-w@tutanota.com>
2025-05-20 01:53:24 +0100

    attempt at getting blink borders working

---
+++ lua/midnight/highlight/plugin/blink-cmp.lua
@@ -0,0 +1,28 @@
+------------------------------------------ BLINK-CMP HIGHLIGHTS ------------------------------------------
+
+-- local p = require('midnight.colors').palette
+-- local c = require('midnight.colors').components
+
+return {
+    -- BlinkCmpMenu
+    BlinkCmpMenuBorder = { link = 'Border' },
+    -- BlinkCmpMenuSelection
+    -- BlinkCmpScrollBarThumb
+    -- BlinkCmpScrollBarGutter
+    -- BlinkCmpLabel
+    -- BlinkCmpLabelDeprecated
+    -- BlinkCmpLabelMatch
+    -- BlinkCmpLabelDetail
+    -- BlinkCmpLabelDescription
+    -- BlinkCmpKind
+    -- BlinkCmpKind<kind>
+    -- BlinkCmpSource
+    -- BlinkCmpGhostText
+    -- BlinkCmpDoc
+    -- BlinkCmpDocBorder
+    -- BlinkCmpDocSeparator
+    -- BlinkCmpDocCursorLine
+    -- BlinkCmpSignatureHelp
+    -- BlinkCmpSignatureHelpBorder
+    -- BlinkCmpSignatureHelpActiveParameter
+}
