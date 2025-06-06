480ad5a252f9fad898ebf27205aee19569724323
fin-w <fin-w@tutanota.com>
2025-06-06 02:15:31 +0100

    colours

--- lua/midnight/highlight/plugin/rainbow-delimiter.lua
+++ lua/midnight/highlight/plugin/rainbow-delimiter.lua
@@ -1,11 +1,11 @@
 ------------------------------------------ RAINBOW-DELIMITER ------------------------------------------
 
--- local p = require('midnight.colors').palette
--- local c = require('midnight.colors').components
+local p = require('midnight.colors').palette
+local c = require('midnight.colors').components
 
 return {
-    RainbowDelimiterRed = { link = 'Border' },
-    RainbowDelimiterOrange = { link = 'Border' },
+    RainbowDelimiterRed = { fg = p.blue[4] },
+    RainbowDelimiterOrange = { fg = p.green[4] },
     RainbowDelimiterYellow = { link = 'Border' },
     RainbowDelimiterGreen = { link = 'Border' },
     RainbowDelimiterBlue = { link = 'Border' },
