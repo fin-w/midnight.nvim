5440705e5deb6d2f0300badb212916c7af527615
fin-w <fin-w@tutanota.com>
2025-06-06 01:54:46 +0100

    test rainbow delimiters customisation

---
+++ lua/midnight/highlight/plugin/rainbow-delimiter.lua
@@ -0,0 +1,11 @@
+------------------------------------------ RAINBOW-DELIMITER ------------------------------------------
+
+-- local p = require('midnight.colors').palette
+-- local c = require('midnight.colors').components
+
+return {
+    RainbowDelimiterA = { link = 'Border' },
+    RainbowDelimiterB = { link = 'Border' },
+    RainbowDelimiterC = { link = 'Border' },
+    RainbowDelimiterD = { link = 'Border' },
+}
